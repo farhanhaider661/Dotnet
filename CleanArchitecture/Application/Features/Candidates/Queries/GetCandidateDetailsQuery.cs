1c347aea5d940faa4c0a642da87788f0eb39d2c2
farhanhaider661 <111038642+farhanhaider661@users.noreply.github.com>
2024-02-27 13:23:12 +0500

    Add files via upload

---
+++ CleanArchitecture/Application/Features/Candidates/Queries/GetCandidateDetailsQuery.cs
@@ -0,0 +1,50 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Candidates.Queries
+{
+    public class GetCandidateDetailsQuery : IRequest<Candidate>
+    {
+        public int CandidateId { get; set; }
+    }
+
+    public class GetCandidateDetailsQueryHandler : IRequestHandler<GetCandidateDetailsQuery, Candidate>
+    {
+        private readonly IVotingSystemDbContext _context;
+
+        public GetCandidateDetailsQueryHandler(IVotingSystemDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Candidate> Handle(GetCandidateDetailsQuery request, CancellationToken cancellationToken)
+        {
+            var candidate = await _context.Candidates
+                .Where(c => c.CandidateId == request.CandidateId)
+                .Select(c => new Candidate
+                {
+                    CandidateId = c.CandidateId,
+                    UserId = c.UserId,
+                    Name = c.Name,
+                    PositionRunningFor = c.PositionRunningFor,
+                    // ... map other properties to the DTO
+                })
+                .SingleOrDefaultAsync(cancellationToken);
+
+            if (candidate == null)
+            {
+                return null;
+            }
+
+            return candidate;
+        }
+    }
+}
