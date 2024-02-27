837636ef7c07bf011348362ea7a63aae9662b68c
farhanhaider661 <111038642+farhanhaider661@users.noreply.github.com>
2024-02-27 13:28:35 +0500

    Add files via upload

---
+++ CleanArchitecture/Application/Features/VoterManagement/Commands/CloseVotingPeriodCommand.cs
@@ -0,0 +1,37 @@
+﻿using Application.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Voting_Management.Commands
+{
+    public class CloseVotingPeriodCommand : IRequest<bool>
+    {
+        public int ElectionId { get; set; }
+    }
+
+    public class CloseVotingPeriodCommandHandler : IRequestHandler<CloseVotingPeriodCommand, bool>
+    {
+        private readonly IVotingSystemDbContext _context;
+
+        public CloseVotingPeriodCommandHandler(IVotingSystemDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Handle(CloseVotingPeriodCommand request, CancellationToken cancellationToken)
+        {
+            var election = await _context.Elections.FindAsync(request.ElectionId);
+            if (election == null) return false;
+
+            election.EndDate = DateTime.UtcNow; // Close the voting period by setting the end date to now
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+    }
+
+}
---
+++ CleanArchitecture/Application/Features/VoterManagement/Commands/OpenVotingPeriodCommand.cs
@@ -0,0 +1,38 @@
+﻿using Application.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Voting_Management.Commands
+{
+    public class OpenVotingPeriodCommand : IRequest<bool>
+    {
+        public int ElectionId { get; set; }
+    }
+
+    public class OpenVotingPeriodCommandHandler : IRequestHandler<OpenVotingPeriodCommand, bool>
+    {
+        private readonly IVotingSystemDbContext _context;
+
+        public OpenVotingPeriodCommandHandler(IVotingSystemDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Handle(OpenVotingPeriodCommand request, CancellationToken cancellationToken)
+        {
+            var election = await _context.Elections.FindAsync(request.ElectionId);
+            if (election == null) return false;
+
+            election.StartDate = DateTime.UtcNow; // Open the voting period by setting the start date to now
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+    }
+
+
+}
