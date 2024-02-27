430a0b062a9b135b5b6af9890269f778105e1f55
farhanhaider661 <111038642+farhanhaider661@users.noreply.github.com>
2024-02-27 13:26:31 +0500

    Add files via upload

---
+++ CleanArchitecture/Application/Features/Voter/Queries/GetVotingHistoryQuery.cs
@@ -0,0 +1,42 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Voter.Queries
+{
+    public class GetVotingHistoryQuery : IRequest<List<Vote>>
+    {
+        public int UserId { get; set; }
+        internal class GetVotingHistoryQueryHandler : IRequestHandler<GetVotingHistoryQuery, List<Vote>>
+        {
+            private readonly IVotingSystemDbContext _context;
+
+            public GetVotingHistoryQueryHandler(IVotingSystemDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<List<Vote>> Handle(GetVotingHistoryQuery request, CancellationToken cancellationToken)
+            {
+                var votes = await _context.Votes
+            .Where(v => v.UserId == request.UserId)
+            .Select(v => new Vote
+            {
+                VoteId = v.VoteId,
+                UserId = v.UserId,
+                CandidateId = v.CandidateId,
+                ElectionId = v.ElectionId,
+                Timestamp = v.Timestamp
+            })
+            .ToListAsync(cancellationToken);
+             return votes;
+            }
+        }
+    }
+}
