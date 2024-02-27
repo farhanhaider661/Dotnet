3b2f8bcc69f72dc11484571651923567511d530c
farhanhaider661 <111038642+farhanhaider661@users.noreply.github.com>
2024-02-27 11:49:12 +0500

    Add files via upload

---
+++ E commerce/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using shopping_cart_backend.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost3000",
+        policyBuilder =>
+        {
+            policyBuilder.WithOrigins("http://localhost:3000")
+                         .AllowAnyHeader()
+                         .AllowAnyMethod();
+        });
+});
+var provider = builder.Services.BuildServiceProvider();
+var config = provider.GetRequiredService<IConfiguration>();
+builder.Services.AddDbContext<ShoppingCartDbContext>(item => item.UseSqlServer(config.GetConnectionString("dbcs")));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("AllowLocalhost3000");
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
