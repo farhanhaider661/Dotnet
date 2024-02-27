33360130fb89b8d3789769dce95d098fdea2e299
farhanhaider661 <111038642+farhanhaider661@users.noreply.github.com>
2024-02-27 13:24:52 +0500

    Add files via upload

---
+++ CleanArchitecture/Application/Features/Voter/Commands/CastVoteCommand.cs
@@ -0,0 +1,42 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Voter.Commands
+{
+    public class CastVoteCommand: IRequest<bool>
+    {
+        public int UserId { get; set; }
+        public int CandidateId { get; set; }
+        public int ElectionId { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+    public class CastVoteCommandHandler : IRequestHandler<CastVoteCommand, bool>
+    {
+        private readonly IVotingSystemDbContext _context;
+
+        public CastVoteCommandHandler(IVotingSystemDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> Handle(CastVoteCommand request, CancellationToken cancellationToken)
+        {
+            var vote = new Vote
+            {
+                UserId = request.UserId,
+                CandidateId = request.CandidateId,
+                ElectionId = request.ElectionId,
+                Timestamp = DateTime.UtcNow // Record the time the vote was cast
+            };
+            _context.Votes.Add(vote);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+
+}
