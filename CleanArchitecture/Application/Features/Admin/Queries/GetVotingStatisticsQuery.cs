a976962687c8f9886373d43b744094b4ccbf622b
farhanhaider661 <111038642+farhanhaider661@users.noreply.github.com>
2024-02-27 12:13:37 +0500

    Add files via upload

---
+++ CleanArchitecture/Application/Features/Admin/Queries/GetVotingStatisticsQuery.cs
@@ -0,0 +1,43 @@
+﻿using Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Admin.Queries
+{
+    public class GetVotingStatisticsQuery : IRequest<VotingStatisticsDto>
+    {
+        // Add properties to filter statistics as needed
+
+        public class GetVotingStatisticsQueryHandler : IRequestHandler<GetVotingStatisticsQuery, VotingStatisticsDto>
+        {
+            private readonly IVotingSystemDbContext _context;
+
+            public GetVotingStatisticsQueryHandler(IVotingSystemDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<VotingStatisticsDto> Handle(GetVotingStatisticsQuery request, CancellationToken cancellationToken)
+            {
+                var statistics = new VotingStatisticsDto
+                {
+                    TotalVotes = await _context.Votes.CountAsync(),
+                };
+
+                return statistics;
+            }
+        }
+    }
+
+    public class VotingStatisticsDto
+    {
+        public int TotalVotes { get; set; }
+        // Add other statistics as needed
+    }
+
+}
