39293490dd9815762dc11f78e7916a711d0ddd42
farhanhaider661 <111038642+farhanhaider661@users.noreply.github.com>
2024-02-27 13:35:24 +0500

    Add files via upload

---
+++ CleanArchitecture/Persistance/ServiceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Application.Interfaces;
+using Domain.Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance
+{
+    public static class ServiceExtensions
+    {
+        public static void AddPersistance(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<VotingSystemDbContext>(option => option.UseSqlServer(
+                config.GetConnectionString("DefaultConnection")
+                ));
+            services.AddScoped<IVotingSystemDbContext, VotingSystemDbContext>();
+        }
+
+    }
+}
