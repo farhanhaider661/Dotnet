cdb9fb8aae915f2f38455c3e0e2e47ea1bd6c0a5
farhanhaider661 <111038642+farhanhaider661@users.noreply.github.com>
2024-02-27 12:16:15 +0500

    Add files via upload

---
+++ CleanArchitecture/Application/Features/Authentication/Queries/CheckUserCredentialsQuery.cs
@@ -0,0 +1,37 @@
+﻿using Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Authenication.Queries
+{
+    public class CheckUserCredentialsQuery: IRequest<int>
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+    public class CheckUserCredentialsQueryHandler : IRequestHandler<CheckUserCredentialsQuery, int>
+    {
+        private readonly IVotingSystemDbContext _context;
+
+        public CheckUserCredentialsQueryHandler(IVotingSystemDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(CheckUserCredentialsQuery request, CancellationToken cancellationToken)
+        {
+            var user=await _context.Users.FirstOrDefaultAsync(u=>u.Username==request.Name && u.Password== request.Password);
+            if (user != null)
+            {
+                return user.UserId;
+            }
+            else { return 0; }
+            
+        }
+    }
+}
---
+++ CleanArchitecture/Application/Features/Authentication/Queries/GetUserProfileQuery.cs
@@ -0,0 +1,32 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Authenication.Queries
+{
+    public class GetUserProfileQuery: IRequest<User>
+    {
+        public int Id { get; set; }
+    }
+    public class GetUserProfileQueryHandler : IRequestHandler<GetUserProfileQuery, User>
+    {
+
+        private readonly IVotingSystemDbContext _context;
+        public GetUserProfileQueryHandler(IVotingSystemDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<User> Handle(GetUserProfileQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _context.Users.Where(x => x.UserId == request.Id).FirstOrDefaultAsync(cancellationToken);
+            return result;
+        }
+    }
+
+}
