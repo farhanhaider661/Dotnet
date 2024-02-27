0b11152b3bcf2229b9a9c9cd748c7123ddee0806
farhanhaider661 <111038642+farhanhaider661@users.noreply.github.com>
2024-02-27 12:15:05 +0500

    Add files via upload

---
+++ CleanArchitecture/Application/Features/Authentication/Commands/ChangePasswordCommand.cs
@@ -0,0 +1,40 @@
+﻿using Application.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Authenication.Commands
+{
+    public class ChangePasswordCommand: IRequest<bool>
+    {
+        public int UserId { get; set; }
+        public string NewPassword { get; set; }
+    }
+    public class ChangePasswordCommandHandler : IRequestHandler<ChangePasswordCommand, bool>
+    {
+        private readonly IVotingSystemDbContext _context;
+
+        public ChangePasswordCommandHandler(IVotingSystemDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Handle(ChangePasswordCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _context.Users.FindAsync(request.UserId);
+
+            if (user == null)
+            {
+                throw new Exception("User not found");
+            }
+
+            user.Password = request.NewPassword;
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
---
+++ CleanArchitecture/Application/Features/Authentication/Commands/RegisterUserCommand.cs
@@ -0,0 +1,42 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Authenication.Commands
+{
+    public class RegisterUserCommand: IRequest<bool>
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public DateTime RegisterationDate  { get; set; }
+    }
+    public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, bool>
+    {
+        private readonly IVotingSystemDbContext context;
+        public RegisterUserCommandHandler(IVotingSystemDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<bool> Handle(RegisterUserCommand request,CancellationToken cancellationToken)
+        {
+            var user = new User()
+            {
+                Username = request.Username,
+                Password = request.Password,
+                EmailAddress = request.Email,
+                Role = request.Role,
+                RegistrationDate = request.RegisterationDate
+            };
+            context.Users.AddAsync(user);
+            await context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
