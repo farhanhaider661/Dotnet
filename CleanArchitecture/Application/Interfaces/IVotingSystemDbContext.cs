bdc3bc6d944b7511c525f4b23abb88649876683b
farhanhaider661 <111038642+farhanhaider661@users.noreply.github.com>
2024-02-27 12:06:32 +0500

    Add files via upload

---
+++ CleanArchitecture/Application/Interfaces/IVotingSystemDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IVotingSystemDbContext
+    {
+        DbSet<Candidate> Candidates { get; set; }
+
+        DbSet<Election> Elections { get; set; }
+
+        DbSet<User> Users { get; set; }
+
+        DbSet<Vote> Votes { get; set; }
+        Task<int> SaveChangesAsync();
+    }
+}
